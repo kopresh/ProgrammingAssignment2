f32af0e9ac9fefc1f090b59b1a0d824b3dcc486a
kp <kp@gmail.com>
2015-08-23 22:13:09 +0530

    kopresh programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This script will create a matrix and cache its inverse
 
-## Write a short comment describing this function
+
+## This function will create a matrix object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function() x
+  setmatrix<-function(solve) m<<- solve
+  getmatrix<-function() m
+  list(set=set, get=get,
+       setmatrix=setmatrix,
+       getmatrix=getmatrix)
 
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
+## This function computes the inverse matrix returned by makeCacheMatrix
+##IF Inverse has already been calcualted, then this function shoud retrieve it from the cache. 
+cacheSolve <- function(x = matrix(), ...) {
         ## Return a matrix that is the inverse of 'x'
+  m<-x$getmatrix()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  matrix<-x$get()
+  m<-solve(matrix, ...)
+  x$setmatrix(m)
+  m
 }
